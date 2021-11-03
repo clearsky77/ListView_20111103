3542f5b078a5b3ebeb6512695755ee271b447ba4
clearsky77 <clearsky3264@gmail.com>
2021-11-03 13:01:58 +0900

    convertView를 tempRow에 옮겨 담아서, 비어있는 상태라면 채우자

--- app/src/main/java/com/clearsky77/listview_20111103/adapters/StudentAdapter.kt
+++ app/src/main/java/com/clearsky77/listview_20111103/adapters/StudentAdapter.kt
@@ -1,11 +1,24 @@
 package com.clearsky77.listview_20111103.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.clearsky77.listview_20111103.R
 import com.clearsky77.listview_20111103.datas.StudentData
 
 class StudentAdapter(
-    val mCotext: Context,
+    val mContext: Context,
     val resId: Int,
-    val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>(){ // 생성할 때 받는 것을 부모 클래스에 보내준다.
+    val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>(mContext, resId, mList){
+                                                         // 생성할 때 받는 것을 부모 클래스에 보내준다.
+        val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow == mInflater.inflate(R.layout.student_list_item,null) // xml을 끌어서 준비할 수 있게 해주세요.
+        }
+    }
 }
