c272c96d60eb95ea62bcc2f927832caf182a64d0
clearsky77 <clearsky3264@gmail.com>
2021-11-03 13:22:21 +0900

    어댑터를 화면에서 사용할 준비 + 리스트뷰의 어댑터로, 작성한 어댑터 연결

--- app/src/main/java/com/clearsky77/listview_20111103/MainActivity.kt
+++ app/src/main/java/com/clearsky77/listview_20111103/MainActivity.kt
@@ -2,10 +2,28 @@ package com.clearsky77.listview_20111103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clearsky77.listview_20111103.adapters.StudentAdapter
+import com.clearsky77.listview_20111103.datas.StudentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<StudentData>() // 그릇을 만든다
+    lateinit var mStudentAdapter: StudentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(StudentData("김갑갑", 1977, "서울시 동대문구"))
+        mStudentList.add(StudentData("김을갑", 1978, "서울시 동대문구"))
+        mStudentList.add(StudentData("김갑병", 1979, "서울시 동대문구"))
+        mStudentList.add(StudentData("김갑을", 1980, "서울시 동대문구"))
+        mStudentList.add(StudentData("김갑갑", 1977, "서울시 동대문구"))
+        mStudentList.add(StudentData("김을을", 1977, "서울시 동대문구"))
+        mStudentList.add(StudentData("김갑갑", 1977, "서울시 동대문구"))
+
+        mStudentAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+        studentListView.adapter = mStudentAdapter
     }
 }
